f224e43995b19992b6d279cfc3b96d03c27d10c4
Dominique <Dominique57@users.noreply.github.com>
2018-05-01 16:59:14 +0200

    Major fix

--- WEBSITE/profile.php
+++ WEBSITE/profile.php
@@ -30,7 +30,7 @@ include_once 'Includes/session.php';
     else
         $power = -1;
 
-    $bdd = Database($database);
+    $bdd = Database();
     try {
         $req = $bdd->prepare("SELECT * FROM informations WHERE id=?");
         $req->execute(array($id));
