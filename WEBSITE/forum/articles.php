7a0e57381cd808fed13f61c67ee0bd7fc729fb05
Dominique <Dominique57@users.noreply.github.com>
2018-05-02 02:14:56 +0200

    Fixed mysql error and thus in script too

--- WEBSITE/forum/articles.php
+++ WEBSITE/forum/articles.php
@@ -58,6 +58,15 @@ include_once '../Includes/session.php';
                 <?php
                 while ($donnees = $req->fetch()) {
                     try {
+                        $req3 = $bdd->prepare("SELECT pseudo, power FROM informations WHERE id=:id LIMIT 1");
+                        $req3->execute(array(
+                            "id" => $donnees['author']));
+                    } catch (PDOException $e) {
+                        print "Erreur !: " . $e->getMessage() . "<br/>";
+                        return;
+                    }
+                    $donnees3 = $req3->fetch();
+                    try {
                         $req2 = $bdd->prepare("SELECT COUNT(id) FROM posts WHERE parentId=:id");
                         $req2->execute(array(
                             "id" => $donnees['id']));
@@ -66,19 +75,20 @@ include_once '../Includes/session.php';
                         return;
                     }
                     $donnees2 = $req2->fetch();
-                    $numbertopic = $donnees2[0];
                     $req2 = null;
                     ?>
                     <div class="w3-container w3-padding w3-margin w3-card" style="border: 1px grey solid" onclick="document.location='<?php echo '/forum/posts.php?id='.$donnees['id']; ?>'">
                         <div class="w3-col w3-right" style="width: 100px;"><span
-                                    class="w3-right"><?php echo $numbertopic; ?> posts</span></div>
+                                    class="w3-right"><?php echo $donnees2[0]; ?> posts</span></div>
                         <div class="w3-rest">
                             <p>
-                                <?php echo $donnees['title'] . '<br>' . $donnees['author'] . '<br>' . $donnees['date']; ?>
+                                <?php echo '<em style="font-size: 20px;">'.$donnees['title'] . '</em><br>' . $donnees3['pseudo'] . '   <img style="width: 70px" src="'.GetPathRank($donnees3['power']).'"><br>' . $donnees['date']; ?>
                             </p>
                         </div>
                     </div>
                     <?php
+                    $req3 = null;
+                    $req2 = null;
                 } ?>
             </div>
         </div>
